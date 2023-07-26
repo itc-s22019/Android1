48118e2c251070500b9a9a4258d246f2abf8492f
itc-s22019 <s22019@std.it-college.ac.jp>
2023-07-26 17:08:22 +0900

    7/26

--- IntentSample/app/src/main/java/jp/ac/std/it_college/s22019/intentsample/MainActivity.kt
+++ IntentSample/app/src/main/java/jp/ac/std/it_college/s22019/intentsample/MainActivity.kt
@@ -2,10 +2,15 @@ package jp.ac.std.it_college.s22019.intentsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jp.ac.std.it_college.s22019.intentsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }

d073f893b1627c28871873e6e6f492d850645ebf
itc-s22019 <s22019@std.it-college.ac.jp>
2023-07-27 14:41:19 +0900

    7.1

--- IntentSample/app/src/main/java/jp/ac/std/it_college/s22019/intentsample/MainActivity.kt
+++ IntentSample/app/src/main/java/jp/ac/std/it_college/s22019/intentsample/MainActivity.kt
@@ -2,6 +2,8 @@ package jp.ac.std.it_college.s22019.intentsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import jp.ac.std.it_college.s22019.intentsample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +14,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.lvMenu.apply {
+            // アダプターをセット
+            adapter = MenuListAdapter(menuList)
+
+            // RecyclerView の表示方法を直接的にする
+            LinearLayoutManager(this@MainActivity).let {
+                // 直線的に並べる
+                layoutManager = it
+                // 区切り線を並べる方向に合わせて表示する
+                addItemDecoration(DividerItemDecoration(this@MainActivity, it.orientation))
+            }
+        }
     }
 }

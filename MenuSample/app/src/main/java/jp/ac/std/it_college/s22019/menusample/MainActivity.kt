777e7660c9a4a2e20a408ccb9ff0a8d616c41e33
itc-s22019 <s22019@std.it-college.ac.jp>
2023-08-08 17:02:34 +0900

    chap08 8/8

--- MenuSample/app/src/main/java/jp/ac/std/it_college/s22019/menusample/MainActivity.kt
+++ MenuSample/app/src/main/java/jp/ac/std/it_college/s22019/menusample/MainActivity.kt
@@ -1,11 +1,61 @@
 package jp.ac.std.it_college.s22019.menusample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import jp.ac.std.it_college.s22019.menusample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val menuList = mutableListOf<jp.ac.std.it_college.s22019.menusample.Menu>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initList(binding.lvMenu)
+    }
+
+    private fun initList(view: RecyclerView) {
+        view.adapter = MenuListAdapter(teishokuList, ::order)
+        val layoutManager = LinearLayoutManager(this)
+        view.layoutManager = layoutManager
+        view.addItemDecoration(DividerItemDecoration(this, layoutManager.orientation))
+    }
+
+    private fun order(name: String, price: Int) {
+        startActivity(Intent(this, MenuThanksActivity::class.java).apply {
+            putExtra("menuName", name)
+            putExtra("menuPrice", price)
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_options_menu_list, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val result = when (item.itemId) {
+            R.id.menuListOptionsTeishoku -> resetMenu(teishokuList)
+
+            R.id.menuListOptionsCurry -> resetMenu(curryList)
+
+            else -> super.onOptionsItemSelected(item)
+        }
+        return result
+    }
+
+    private fun resetMenu(list: List<jp.ac.std.it_college.s22019.menusample.Menu>): Boolean {
+        menuList.clear()
+        menuList.addAll(list)
+        binding.lvMenu.adapter?.notifyDataSetChanged()
+        return true
     }
 }
---
+++ MenuSample/app/src/main/java/jp/ac/std/it_college/s22019/menusample/MenuThanksActivity.kt
@@ -0,0 +1,29 @@
+package jp.ac.std.it_college.s22019.menusample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jp.ac.std.it_college.s22019.menusample.databinding.ActivityMenuThanksBinding
+
+class MenuThanksActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMenuThanksBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMenuThanksBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 前の画面から渡ってくるであろうデータを取り出す
+        val menuName = intent.getStringExtra("menuName") ?: ""
+        val menuPrice = intent.getIntExtra("menuPrice", 0)
+
+        // データをセット
+        binding.tvMenuName.text = menuName
+        binding.tvMenuPrice.text = "%,d".format(menuPrice)
+
+        // リストに戻るボタンのイベント設定
+        binding.btThxBack.setOnClickListener {
+            finish()
+        }
+
+    }
+}
